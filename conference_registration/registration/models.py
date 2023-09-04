0e6d151880e718b7f896261d84fdfbf2c59ef825
Mfundo-debug <diditmfundo@gmail.com>
2023-09-04 18:19:25 +0200

    models.py file for the app

---
+++ conference_registration/registration/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email_address = models.EmailField(max_length=254)
+    residential_address = models.CharField(max_length=100)
+    student_identity_number = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
