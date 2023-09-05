152c72fd11f159f9ff0394a1bb7e3d4a4812ba3e
TonyLapuken <87187623+TonyLapuken@users.noreply.github.com>
2023-09-05 15:45:40 +0200

    fixed css code and added div center

--- conference_registration/registration-frontend/src/components/RegistrationForm.js
+++ conference_registration/registration-frontend/src/components/RegistrationForm.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import PersonalInfo from './PersonalInfo';
-import styles from './RegistrationForm.css';
+import React from "react";
+import PersonalInfo from "./PersonalInfo";
+import "./RegistrationForm.css";
 
-function RegistrationForm(){
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        //Placeholder: Implement form submission logic here
-        //console.log('Form submitted', formData);
-    };
+function RegistrationForm() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //Placeholder: Implement form submission logic here
+    //console.log('Form submitted', formData);
+  };
 
-
-    return (
-        <div className={styles["registration-form"]}>
-          <h1>User Registration</h1>
-          <form onSubmit={handleSubmit}>
-            <PersonalInfo />
-            <button type="submit">Submit</button>
-          </form>
-        </div>
-      );
-    }
+  return (
+    <div className={"registration-form"}>
+      <h1>User Registration</h1>
+      <form onSubmit={handleSubmit}>
+        <PersonalInfo />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
 
 export default RegistrationForm;
