d687b95ad53221010423f85ed813cf7505922031
Mfundo-debug <diditmfundo@gmail.com>
2023-09-10 14:55:59 +0200

    setup file for easy installation

--- conference_registration/registration/views.py
+++ conference_registration/registration/views.py
@@ -4,6 +4,9 @@ from .models import User
 from .serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
 class UserRegistrationView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -31,3 +34,28 @@ class UserRegistrationView(generics.CreateAPIView):
     def post(self, request, *args, **kwargs):
         #Handles POST requests here
         return super().post(request, *args, **kwargs)
+    
+@csrf_exempt
+def user_registration(request):
+    if request.method == 'POST':
+        #Retrieve data from the POST request body
+        data = json.loads(request.body)
+        first_name = data.get('first_name', "")
+        last_name = data.get('last_name', "")
+        email_address = data.get('email_address', "")
+        residential_address = data.get('residential_address', "")
+        student_identity_number = data.get('student_identity_number', "")
+
+        user = User(
+            first_name=first_name,
+            last_name=last_name,
+            email_address=email_address,
+            residential_address=residential_address,
+            student_identity_number=student_identity_number
+        )
+        user.save()
+        #Return a JSON response with a status code of 201 created
+        return JsonResponse({'message': 'User created successfully!'}, status=status.HTTP_201_CREATED)
+    else:
+        #Return a JSON response with a status code of 405 method not allowed
+        return JsonResponse({'message': 'This is a POST request!'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
