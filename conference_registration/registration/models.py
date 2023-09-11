cf82eb0ab4d5fab34f7e6a2cb4d9e84b8a4873b9
Mfundo-debug <diditmfundo@gmail.com>
2023-09-11 08:25:57 +0200

    added 5 feats, and fixed system func

--- conference_registration/registration/models.py
+++ conference_registration/registration/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.utils import timezone
 
 # Create your models here.
 class User(models.Model):
@@ -21,12 +22,17 @@ class User(models.Model):
     residential_address = models.CharField(max_length=100)
     student_identity_number = models.CharField(max_length=10, unique=True)
     system_email = models.EmailField(max_length=254, unique=True, blank=True, null=True)
+    dob = models.DateField(default=timezone.now)
+    gender = models.CharField(max_length=10, null=True)
+    country = models.CharField(max_length=30, null=True)
+    nationality = models.CharField(max_length=30, null=True)
+    age = models.IntegerField(null=True)
 
 
     def generate_system_email(instance):
-        first_name = instance.first_name.replace(' ', '')
+        first_name = instance.first_name.split()[0]
         last_name = instance.last_name
-        initials = first_name[0] + last_name[0]
+        initials = first_name[0]
         system_email = f"{last_name}{initials}@SmartSystem.com"
         count = User.objects.filter(system_email=system_email).count()
         if count > 0:
