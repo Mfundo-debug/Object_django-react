5c5575444eec462fa09de55400b39f6e651350f3
Mfundo-debug <diditmfundo@gmail.com>
2023-09-13 00:19:19 +0200

    looking for user_id ?

--- conference_registration/registration-frontend/src/components/ConfirmationPage.js
+++ conference_registration/registration-frontend/src/components/ConfirmationPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './RegistrationForm.css';
 
-function ConfirmationPage() {
+function ConfirmationPage(props) {
+  const { user_id } = props; // Destructure the user_id prop
+
   return (
     <div className='confirmation-page'>
       <h1>Registration Successful</h1>
       <div className='confirmation-message'>
         <p>Thank you for registering for the conference. Your registration is now complete.</p>
+        <p>Your user ID is: {user_id}</p> {/* Render the user_id */}
         <p>We look forward to seeing you at the conference.</p>
       </div>
     </div>
