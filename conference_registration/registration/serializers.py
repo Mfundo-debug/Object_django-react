aeb2226f940510c88a1132fcfef7ddbe78d0ec2d
Mfundo-debug <diditmfundo@gmail.com>
2023-09-14 11:30:28 +0200

    dob serializer and representation

--- conference_registration/registration/serializers.py
+++ conference_registration/registration/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    dob = serializers.DateField()
     class Meta:
         model = User
         fields = '__all__'
+
+    def to_representation(self, instance):
+        return super().to_representation(instance)
