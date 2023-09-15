70ff35349229f0663c4f377a2e26643050013a39
Mfundo-debug <diditmfundo@gmail.com>
2023-09-15 12:28:20 +0200

    handle env vars

--- conference_registration/registration-frontend/src/components/Configure.js
+++ conference_registration/registration-frontend/src/components/Configure.js
@@ -1,21 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC_nQEX7Wb06eQ9st7oY_7Du7QrZPwgwuU",
-  authDomain: "authenticator-fce4c.firebaseapp.com",
-  projectId: "authenticator-fce4c",
-  storageBucket: "authenticator-fce4c.appspot.com",
-  messagingSenderId: "323903073089",
-  appId: "1:323903073089:web:1ce7e54f5fef268528cfc0",
-  measurementId: "G-SWT9LEMKN2"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export {app};
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+export { auth, provider };
