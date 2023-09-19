fb7a19ec4a237ba67265b35d1631bd4c4b840124
Mfundo-debug <diditmfundo@gmail.com>
2023-09-19 18:19:56 +0200

    added one link for updating

--- conference_registration/registration/urls.py
+++ conference_registration/registration/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('register/', views.UserRegistrationView.as_view(), name='user-registration'),
     path('register/options/', views.options_view, name='options_view'),
+    # create a new URL for calculating student information
+    path('api/calculate_student_info/', views.CalculateStudentInfoView.as_view(), name='calculate-student-info'),
 ]
