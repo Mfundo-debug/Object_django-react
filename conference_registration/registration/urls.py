0f6529d68182f719a14b927a5fb778ddde8b29b5
Mfundo-debug <diditmfundo@gmail.com>
2023-09-26 01:49:18 +0200

    api for validation

--- conference_registration/registration/urls.py
+++ conference_registration/registration/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/options/', views.options_view, name='options_view'),
     # create a new URL for calculating student information
     path('api/calculate_student_info/', views.CalculateStudentInfoView.as_view(), name='calculate-student-info'),
+    path('validate/', views.validate_user, name='validate-user'),
 ]
